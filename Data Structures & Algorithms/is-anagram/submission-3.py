4b5012ffecaed362baca76071f22584faa99c542
magilas69 <135997618+magilas69@users.noreply.github.com>
2026-06-14 21:51:47 +0800

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if (len(t) != len(s)):
+            return False
+        first = {}
+        second = {}
+
+        for i in s:
+            if (i not in first):
+                first[i] = 1
+            else:
+                first[i] += 1
+        for j in t:
+            if (j not in second):
+                second[j] = 1
+            else:
+                second[j] += 1
+        if (first == second):
+            return True
+        return False
